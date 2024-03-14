5157b89bb52279970fd51883441a2a1a45dde094
ayushig05 <ayushigupta0415@gmail.com>
2024-03-14 18:17:43 +0530

    test commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,30 @@ const About = () => {
           About Us
         </h1>
         <p className=" font-medium leading-6 text-gray-500 text-2xl ml-2 ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eaque sit ullam quasi consequuntur ut officia non vel minima, omnis, provident dignissimos veritatis, blanditiis quos incidunt perspiciatis dolore facere mollitia? Molestiae id quas nam laborum. Minima porro veniam illum eius, dolore deserunt, pariatur, nobis delectus consequatur nam placeat ut reprehenderit. Alias beatae reiciendis quisquam voluptate tenetur blanditiis, consequuntur molestiae quia doloremque repellendus architecto quas ab quibusdam hic atque ipsum deleniti vel inventore ratione magnam nam possimus nostrum sequi. Doloribus, laudantium quasi! Vero laboriosam accusantium, a laudantium ab ea atque aperiam quam facere blanditiis impedit voluptates perferendis assumenda, totam neque tempore, quia vitae iste. Repudiandae, tenetur! Voluptate dolorum a illo et explicabo blanditiis dolor inventore ullam maiores error libero nostrum labore veniam qui, eaque voluptas, alias repudiandae saepe eum! Nostrum vero laudantium voluptate quas, ad fugit voluptas maiores libero? Tempora vero impedit a laborum ratione hic? Eligendi nesciunt perspiciatis quasi laborum deserunt, eveniet commodi voluptate voluptatem hic ullam facilis sunt omnis. Pariatur aspernatur, quis perferendis rem magni minus dolorum illo fuga dolores doloribus voluptatum non veritatis totam possimus repudiandae vel at aperiam qui architecto quidem quo, deleniti unde itaque! Deserunt corrupti temporibus possimus asperiores ipsa nesciunt doloremque commodi, nisi consequuntur! Corporis.
+          test-commit Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem eaque sit ullam quasi consequuntur ut officia non vel
+          minima, omnis, provident dignissimos veritatis, blanditiis quos
+          incidunt perspiciatis dolore facere mollitia? Molestiae id quas nam
+          laborum. Minima porro veniam illum eius, dolore deserunt, pariatur,
+          nobis delectus consequatur nam placeat ut reprehenderit. Alias beatae
+          reiciendis quisquam voluptate tenetur blanditiis, consequuntur
+          molestiae quia doloremque repellendus architecto quas ab quibusdam hic
+          atque ipsum deleniti vel inventore ratione magnam nam possimus nostrum
+          sequi. Doloribus, laudantium quasi! Vero laboriosam accusantium, a
+          laudantium ab ea atque aperiam quam facere blanditiis impedit
+          voluptates perferendis assumenda, totam neque tempore, quia vitae
+          iste. Repudiandae, tenetur! Voluptate dolorum a illo et explicabo
+          blanditiis dolor inventore ullam maiores error libero nostrum labore
+          veniam qui, eaque voluptas, alias repudiandae saepe eum! Nostrum vero
+          laudantium voluptate quas, ad fugit voluptas maiores libero? Tempora
+          vero impedit a laborum ratione hic? Eligendi nesciunt perspiciatis
+          quasi laborum deserunt, eveniet commodi voluptate voluptatem hic ullam
+          facilis sunt omnis. Pariatur aspernatur, quis perferendis rem magni
+          minus dolorum illo fuga dolores doloribus voluptatum non veritatis
+          totam possimus repudiandae vel at aperiam qui architecto quidem quo,
+          deleniti unde itaque! Deserunt corrupti temporibus possimus asperiores
+          ipsa nesciunt doloremque commodi, nisi consequuntur! Corporis.
         </p>
-        
       </div>
     </div>
   );
