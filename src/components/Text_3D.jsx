1a2fa995097248f7fcc455519ace2095010539aa
YASH SINGH <123338008+yashsingh9651@users.noreply.github.com>
2024-03-14 18:09:56 +0530

    React-3-fiber

---
+++ src/components/Text_3D.jsx
@@ -0,0 +1,15 @@
+import { Text } from "@react-three/drei";
+import React from "react";
+
+const Text_3D = () => {
+  return (
+    <>
+      <Text>
+        Dexte------rix-4.0
+        <meshBasicMaterial color="white" />
+      </Text>
+    </>
+  );
+};
+
+export default Text_3D;
