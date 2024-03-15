e16b5fac0edc1873f62587916d6906ea7bfc1659
Ayushi Gupta <ayushigupta0415@gmail.com>
2024-03-15 11:40:50 +0530

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,33 +3,24 @@ import React from "react";
 const About = () => {
   return (
     <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 relative">
-      <div className="w-full flex flex-col justify-center items-center relative z-10">
+      <div className="w-full flex flex-col justify-center items-center relative z-10 font-Phudu">
         <h1 className="text-3xl lg:text-4xl flex justify-center font-bold leading-9 text-white pb-4">
           About Us
         </h1>
-        <p className="font-medium leading-6 text-gray-500 text-sm lg:text-xl ml-2">
-          test-commit Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem eaque sit ullam quasi consequuntur ut officia non vel
-          impedit voluptates perferendis assumenda, totam neque tempore, quia
-          vitae iste. Repudiandae, tenetur! Voluptate dolorum a illo et
-          explicabo blanditiis dolor inventore ullam maiores error libero
-          nostrum labore
+        <p className="font-light leading-6 text-white text-sm lg:text-lg ml-2">
+          Welcome to Dexterix 4.0, Galgotias University's premier hackathon! Organized in collaboration between Team TechnoJam, the Technical Club under GU Student Council, and the University’s School of Computing Science and Engineering, Dexterix 4.0 promises to be an exhilarating journey into the world of technology and innovation.
         </p>
-        <p className="font-medium leading-6 text-gray-500 text-sm lg:text-xl mt-3 ml-2">
-          test-commit Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem eaque sit ullam quasi consequuntur ut officia non vel
-          impedit voluptates perferendis assumenda, totam neque tempore, quia
-          vitae iste. Repudiandae, tenetur! Voluptate dolorum a illo et
-          explicabo blanditiis dolor inventore ullam maiores error libero
-          nostrum labore
+        <p className="font-light leading-6 text-white text-sm lg:text-lg mt-3 ml-2">
+          Building upon the success of its predecessors, Dexterix 4.0 invites participants from over 100+ colleges and universities across India to showcase their skills and creativity in a 36-hour coding marathon. This year, we are proud to introduce two tracks: corporate level and student level, opening doors for graduates and professionals in the tech industry to compete alongside students for exciting prizes and opportunities.
         </p>
-        <p className="font-medium leading-6 text-gray-500 text-sm lg:text-xl mt-3 ml-2">
-          test-commit Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem eaque sit ullam quasi consequuntur ut officia non vel
-          impedit voluptates perferendis assumenda, totam neque tempore, quia
-          vitae iste. Repudiandae, tenetur! Voluptate dolorum a illo et
-          explicabo blanditiis dolor inventore ullam maiores error libero
-          nostrum labore
+        <p className="font-light leading-6 text-white text-sm lg:text-lg mt-3 ml-2">
+          With the unwavering support of the School of Computing Science and Engineering, Dexterix 4.0 aims to push the boundaries of innovation and collaboration. From the bustling campus of GU to the guidance of esteemed faculty members, every aspect of Dexterix is fueled by the spirit of technological advancement.
+        </p>
+        <p className="font-light leading-6 text-white text-sm lg:text-lg mt-3 ml-2">
+          Join us as we embrace the limitless possibilities of tech in Greater Noida and beyond. Together, let’s embark on a journey of creation, learning, and growth. From humble beginnings in 2024, Dexterix has become a beacon of inspiration for thousands, igniting passions and sparking entrepreneurial dreams.
+        </p>
+        <p className="font-light leading-6 text-white text-sm lg:text-lg mt-3 ml-2">
+          As we launch into our fourth iteration, Dexterix 4.0 promises to surpass all expectations, delivering an experience that is bigger, better, and more transformative than ever before.
         </p>
       </div>
     </div>
