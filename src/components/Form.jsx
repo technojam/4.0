06fdc1e0bcfef8dda5fd2fcdc763a17cc0079913
YASH SINGH <123338008+yashsingh9651@users.noreply.github.com>
2024-03-17 21:35:17 +0530

    Bug Fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1078,11 +1078,3 @@ const BottomGradient = () => {
     </>
   );
 };
-
-const LabelInputContainer = ({ children, className }) => {
-  return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
-    </div>
-  );
-};
