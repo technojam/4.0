5d753ba271f8dc34ddebeefb55672a0ba9c125f4
shiwangi-upadhyay <shiwangiupadhyay332@gmail.com>
2024-03-20 14:12:25 +0530

    Sponsors Updated

--- src/components/MovingCards.jsx
+++ src/components/MovingCards.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { cn } from './utils/cn';
+import React, { useEffect, useState, useRef } from "react";
+import { cn } from "./utils/cn";
 
 const MovingCards = ({
   items,
-  direction = 'left',
-  speed = 'fast',
+  direction = "left",
+  speed = "fast",
   pauseOnHover = true,
   className,
 }) => {
@@ -35,22 +35,28 @@ const MovingCards = ({
 
   const getDirection = () => {
     if (containerRef.current) {
-      if (direction === 'left') {
-        containerRef.current.style.setProperty('--animation-direction', 'forwards');
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
       } else {
-        containerRef.current.style.setProperty('--animation-direction', 'reverse');
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
       }
     }
   };
 
   const getSpeed = () => {
     if (containerRef.current) {
-      if (speed === 'fast') {
-        containerRef.current.style.setProperty('--animation-duration', '20s');
-      } else if (speed === 'normal') {
-        containerRef.current.style.setProperty('--animation-duration', '40s');
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
       } else {
-        containerRef.current.style.setProperty('--animation-duration', '80s');
+        containerRef.current.style.setProperty("--animation-duration", "80s");
       }
     }
   };
@@ -59,27 +65,29 @@ const MovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        'scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]',
+        "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
       <ul
         ref={scrollerRef}
         className={cn(
-          'flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap',
-          start && 'animate-scroll',
-          pauseOnHover && 'hover:[animation-play-state:paused]'
+          "flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] h-[240px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6
+             md:w-[300px] md:h-[250px]  "
             style={{
-              background: 'linear-gradient(180deg, var(--slate-800), var(--slate-900)',
+              background:
+                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
             key={item.name}
           >
-            <blockquote>
+            {/*<blockquote>
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
@@ -97,7 +105,12 @@ const MovingCards = ({
                   </span>
                 </span>
               </div>
-            </blockquote>
+            </blockquote>*/}
+            <img
+              className="w-full h-full object-contain"
+              src={item.src}
+              alt=""
+            />
           </li>
         ))}
       </ul>
