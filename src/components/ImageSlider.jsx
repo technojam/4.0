4819e5c63d652a5fea97f787e3fe2bbc3ffec5ba
shiwangi-upadhyay <shiwangiupadhyay332@gmail.com>
2024-03-27 14:03:25 +0530

    Past Events Photos Added

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -6,13 +6,40 @@ import { RxDotFilled } from "react-icons/rx";
 function ImageSlider() {
   const slides = [
     {
-      url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+      url: "/Dex-1.1.png",
     },
     {
-      url: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+      url: "/Dex-1.2.png",
     },
     {
-      url: "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg",
+      url: "/Dex-2.1.png",
+    },
+    {
+      url: "/Dex-2.2.png",
+    },
+    {
+      url: "/Dex-2.3.png",
+    },
+    {
+      url: "/Dex-3.1.png",
+    },
+    {
+      url: "/Dex-3.2.png",
+    },
+    {
+      url: "/Dex-3.2.png",
+    },
+    {
+      url: "/nasa-1.1.png",
+    },
+    {
+      url: "/nasa-2.1.png",
+    },
+    {
+      url: "/nasa-2.2.png",
+    },
+    {
+      url: "/alexa-student-day.png",
     },
   ];
 
@@ -35,13 +62,13 @@ function ImageSlider() {
   };
 
   return (
-    <div id="events" className="max-w-[900px] h-[500px] w-full m-auto px-4 relative group mb-52">
-      <h1 className="text-[#daa520] text-4xl font-Phudu font-medium flex justify-center mb-5">
+    <div id="events" className="max-w-[900px] h-[270px] md:h-[600px] lg:h-[610px] w-full m-auto px-4 relative group mb-52">
+      <h1 className="text-[#daa520] text-3xl md:text-4xl font-Phudu font-medium flex justify-center mb-5">
         Our Past Events
       </h1>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        className="w-full h-full  rounded-2xl bg-center bg-cover duration-500"
       ></div>
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 mt-16 bg-black/20 text-[#daa520] cursor-pointer">
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
@@ -56,7 +83,7 @@ function ImageSlider() {
             onClick={() => goToSlide(slideIndex)}
             className="text-2xl cursor-pointer"
           >
-            <RxDotFilled className="text-[#daa520]" />
+            <RxDotFilled className="text-[#daa520] text-[16px] md:text-2xl" />
           </div>
         ))}
       </div>
